087fee36c84c474daf90c32bd1f6e31e347c48f0
kluangkhflemingcollegec <kluangkh@flemingcollege.ca>
2021-01-23 17:32:48 -0500

    Earthquake Markers

--- js/script.js
+++ js/script.js
@@ -155,3 +155,33 @@ function degreeMap() {
     { lat: 46.622078, lng: -81.455217 },
     { lat: 51.104, lng: -94.288 },
   ];
+
+function earthquakeMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 2,
+    center: new google.maps.LatLng(2.8, -187.3),
+    mapTypeId: "satellite",
+  });
+  // Create a <script> tag and set the USGS URL as the source.
+  const script = document.createElement("script");
+  // This example uses a local copy of the GeoJSON stored at
+  // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
+  script.src =
+    "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp";
+  document.getElementsByTagName("head")[0].appendChild(script);
+}
+
+// Loop through the results array and place a marker for each
+// set of coordinates.
+const eqfeed_callback = function (results) {
+  for (let i = 0; i < results.features.length; i++) {
+    const coords = results.features[i].geometry.coordinates;
+    const latLng = new google.maps.LatLng(coords[1], coords[0]);
+    const image = "images/flag.png"
+    new google.maps.Marker({
+      position: latLng,
+      map: map,
+      icon: image,
+    });
+  }
+};
