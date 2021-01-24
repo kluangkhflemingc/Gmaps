fc69907b187804e0818fbdc82d66fd1214aff947
kluangkhflemingcollegec <kluangkh@flemingcollege.ca>
2021-01-24 10:12:11 -0500

    Update

--- js/script.js
+++ js/script.js
@@ -156,21 +156,6 @@ function degreeMap() {
     { lat: 51.104, lng: -94.288 },
   ];
 
-function earthquakeMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 2,
-    center: new google.maps.LatLng(2.8, -187.3),
-    mapTypeId: "satellite",
-  });
-  // Create a <script> tag and set the USGS URL as the source.
-  const script = document.createElement("script");
-  // This example uses a local copy of the GeoJSON stored at
-  // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-  script.src =
-    "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp";
-  document.getElementsByTagName("head")[0].appendChild(script);
-}
-
 function heatMap() {
   newheatmap = new google.maps.Map(document.getElementById("map"), {
     zoom: 2,
@@ -193,12 +178,6 @@ const eqfeed_callback = function (results) {
   for (let i = 0; i < results.features.length; i++) {
     const coords = results.features[i].geometry.coordinates;
     const latLng = new google.maps.LatLng(coords[1], coords[0]);
-    const image = "images/flag.png"
-    new google.maps.Marker({
-      position: latLng,
-      map: map,
-      icon: image,
-    });
     heatmapData.push(latLng);
   }
   const heatmap = new google.maps.visualization.HeatmapLayer({
